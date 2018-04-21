4600169419af26b5922740bb566be08b7a44d5ec
Jeff <jmage619@gmail.com>
2018-04-21 10:26:48 -0700

    add indenting for readability

--- calc.asm
+++ calc.asm
@@ -1,101 +1,102 @@
-.word $0801
-*=$0801
-.byte $0c, $08, $0a, $00, $9e, $20
-.byte $34, $30, $39, $36, $00, $00
-.byte $00
-.dsb $1000 - *
+chrout     =$ffd2
+chrin      =$ffcf
 
-chrout = $ffd2
-chrin = $ffcf
+          .(
+          .word $0801
+          *=$0801
+          .byte $0c, $08, $0a, $00, $9e, $20
+          .byte $34, $30, $39, $36, $00, $00
+          .byte $00
+          .dsb $1000 - *
 
-.(
-; print prompt
-ldx #0
-ploop lda prompt,x
-jsr chrout
-inx
-cpx #7
-bne ploop
+          ; print prompt
+          ldx #0
+ploop     lda prompt,x
+          jsr chrout
+          inx
+          cpx #7
+          bne ploop
 
-; get number from user
-ldy #$00
-rloop jsr chrin
-and #$0F
-sta input,y
-iny
-cmp #$0d
-bne rloop
+          ; get number from user
+          ldy #$00
+rloop     jsr chrin
+          and #$0F
+          sta input,y
+          iny
+          cmp #$0d
+          bne rloop
 
-; print new line
-lda #$0d
-jsr chrout
+          ; print new line
+          lda #$0d
+          jsr chrout
 
-; set y to last digit index
-dey
-dey
-ldx #$00
+          ; set y to last digit index
+          dey
+          dey
+          ldx #$00
 
-; loop digits and aggregate
-; value into data8
-next_digit lda coef,x
-stx coef_pos
-tax
-lda input,y
-sty in_pos
-tay
+          ; loop digits and aggregate
+          ; value into data8
+next_digit
+          lda coef,x
+          stx coef_pos
+          tax
+          lda input,y
+          sty in_pos
+          tay
 
-jsr mult8
-clc
-adc data8
-sta data8
-ldx coef_pos
-ldy in_pos
-inx
-dey
-bpl next_digit
+          jsr mult8
+          clc
+          adc data8
+          sta data8
+          ldx coef_pos
+          ldy in_pos
+          inx
+          dey
+          bpl next_digit
 
-rts
+          rts
 
-prompt .byte "input: "
-coef .byte 1, 10, 100
+prompt    .byte "input: "
+coef      .byte 1, 10, 100
 
-input .byte $00, $00, $00, $00, $00
-in_pos .byte $00
-coef_pos .byte $00
-data8 .byte $00
-data .word $0000
-.)
+input     .byte $00, $00, $00, $00, $00
+in_pos    .byte $00
+coef_pos  .byte $00
+data8     .byte $00
+data      .word $0000
+          .)
 
 ; mult8 multiplies two 8 bit integers
 ; input args are x and y registers
 ; return value to a register
 ; a,y are modified
-mult8
-.(
-lda #0
-sta ret_val
-stx input_a
 
-shift_b tya
-cmp #$0
-beq return
-lsr
-tay
+mult8     .(
+          lda #0
+          sta ret_val
+          stx input_a
 
-bcs accum
+shift_b   tya
+          cmp #$0
+          beq return
+          lsr
+          tay
 
-shift_a asl input_a
-jmp shift_b
+          bcs accum
 
-return lda ret_val
-rts
+shift_a   asl input_a
+          jmp shift_b
 
-accum lda ret_val
-clc
-adc input_a
-sta ret_val
-jmp shift_a
+return    lda ret_val
+          rts
 
-input_a .byte 0
-ret_val .byte 0
-.)
+accum     lda ret_val
+          clc
+          adc input_a
+          sta ret_val
+          jmp shift_a
+
+input_a   .byte 0
+ret_val   .byte 0
+          .)
